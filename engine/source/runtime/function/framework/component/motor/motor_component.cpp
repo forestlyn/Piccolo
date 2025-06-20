8bef9b0a5374a804944647e834c16625d29c8db6
forestlyn <yang2797817213@qq.com>
2025-06-20 10:49:03 +0800

    add field

--- engine/source/runtime/function/framework/component/motor/motor_component.cpp
+++ engine/source/runtime/function/framework/component/motor/motor_component.cpp
@@ -131,7 +131,9 @@ namespace Piccolo
             if ((unsigned int)GameCommand::jump & command)
             {
                 m_jump_state                  = JumpState::rising;
-                m_vertical_move_speed         = Math::sqrt(m_motor_res.m_jump_height * 2 * gravity);
+                m_vertical_move_speed         = m_motor_res.m_jump_speed;
+                //std::string str = std::to_string(m_motor_res.m_jump_speed);
+                //LOG_INFO("m_motor_res.m_jump_speed:"+str);
                 m_jump_horizontal_speed_ratio = m_move_speed_ratio;
             }
             else
